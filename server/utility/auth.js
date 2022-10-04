e8bd0769ea7946444d492ded3eb5e60efe0b81eb
Alessandro D'Antoni <alexdant91@gmail.com>
2022-10-04 12:27:05 +0200

    Comments added

--- server/utility/auth.js
+++ server/utility/auth.js
@@ -2,6 +2,11 @@ const jwt = require('jsonwebtoken');
 
 const { SERVER_PRIVATE_KEY } = process.env;
 
+/**
+ * Ottengo un nuovo token per l'utente
+ * @param {object} payload Oggetto da criptare
+ * @returns {string} Il token di sicurezza
+ */
 const genToken = (payload) => {
   try {
     return jwt.sign(payload, SERVER_PRIVATE_KEY);
@@ -10,6 +15,11 @@ const genToken = (payload) => {
   }
 };
 
+/**
+ * Verifico l'effettiva validità del token
+ * @param {string} token Il token da verificare
+ * @returns {payload|Error} Il payload decriptato oppure un errore se il token non è valido
+ */
 const verifyToken = (token) => {
   try {
     return jwt.verify(token, SERVER_PRIVATE_KEY); // -> new Error() || payload -> { _id: user._id, email: user.email }
